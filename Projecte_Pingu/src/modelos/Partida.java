91bf79c996a60f3632a15d6c40360ce3f5ed04f0
Marcelo_Renteria <fmrrensu2004@alumnes.ilerna.com>
2026-02-25 15:44:13 +0100

    Creación de partida.java y tablero.java

---
+++ Projecte_Pingu/src/modelos/Partida.java
@@ -0,0 +1,70 @@
+package modelos;
+
+import java.util.ArrayList;
+
+public class Partida {
+	
+	//ATRIBUTOS
+	protected Tablero tablero;
+	protected ArrayList<jugador> jugador;
+	protected int turnos;
+	protected int jugadorActual;
+	protected boolean fin;
+	protected jugador ganador;
+	
+	//CONSTRUCTOR
+	public Partida(Tablero tablero, ArrayList<modelos.jugador> jugador, int turnos, int jugadorActual, boolean fin,
+			modelos.jugador ganador) {
+		this.tablero = tablero;
+		this.jugador = jugador;
+		this.turnos = turnos;
+		this.jugadorActual = jugadorActual;
+		this.fin = fin;
+		this.ganador = ganador;
+	}
+	
+	//GETTERS Y SETTERS
+	public Tablero getTablero() {
+		return tablero;
+	}
+	public void setTablero(Tablero tablero) {
+		this.tablero = tablero;
+	}
+	public ArrayList<jugador> getJugador() {
+		return jugador;
+	}
+	public void setJugador(ArrayList<jugador> jugador) {
+		this.jugador = jugador;
+	}
+	public int getTurnos() {
+		return turnos;
+	}
+	public void setTurnos(int turnos) {
+		this.turnos = turnos;
+	}
+	public int getJugadorActual() {
+		return jugadorActual;
+	}
+	public void setJugadorActual(int jugadorActual) {
+		this.jugadorActual = jugadorActual;
+	}
+	public boolean isFin() {
+		return fin;
+	}
+	public void setFin(boolean fin) {
+		this.fin = fin;
+	}
+	public jugador getGanador() {
+		return ganador;
+	}
+	public void setGanador(jugador ganador) {
+		this.ganador = ganador;
+	}
+	
+	//MÉTODOS
+	
+	
+
+	
+
+}
---
+++ Projecte_Pingu/src/modelos/Tablero.java
@@ -0,0 +1,32 @@
+package modelos;
+import java.util.ArrayList;
+
+public class Tablero {
+	//ATRIBUTOS
+	protected ArrayList<casilla> casilla;
+	
+	//CONSTRUCTOR
+	public Tablero(ArrayList<casilla> casilla) {
+		this.casilla = casilla;
+	}
+	
+	//GETTERS Y SETTERS
+	public ArrayList<casilla> getCasilla() {
+		return casilla;
+	}
+
+	public void setCasilla(ArrayList<casilla> casilla) {
+		this.casilla = casilla;
+	}
+	
+	//MÉTODOS
+	public void actualizarTablero() {
+		
+	}
+	
+	
+	
+	
+	
+
+}
