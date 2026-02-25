e39f2bfd813fe5e416b8450a7633a95e3aa8948f
DenisTineo <frezzado@alumnes.ilerna.com>
2026-02-25 15:46:10 +0100

    Creación casillas
    Creadas las clases de Agujero, Oso, Trineo, SueloQuebradizo, Evento.
    Creación de la clase abstracta casilla

---
+++ Projecte_Pingu/src/modelos/Agujero.java
@@ -0,0 +1,13 @@
+package modelos;
+
+public class Agujero extends casilla {
+	//CONSTRUCTOR
+	public Agujero(int pos) {
+		super(pos);
+	}
+	
+	@Override
+	public void realizarAccion(Partida partida, jugador jugador) {
+		
+	}
+}
---
+++ Projecte_Pingu/src/modelos/SueloQuebradizo.java
@@ -0,0 +1,13 @@
+package modelos;
+
+public class SueloQuebradizo extends casilla {
+	//CONSTRUCTOR
+	public SueloQuebradizo(int pos) {
+		super(pos);
+	}
+	
+	@Override
+	public void realizarAccion(Partida partida, jugador jugador) {
+		
+	}
+}
