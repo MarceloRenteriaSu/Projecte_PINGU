d935e69f8338efc9dfb9334a863b4bf5478bc316
DenisTineo <frezzado@alumnes.ilerna.com>
2026-02-25 19:35:30 +0100

    Update Pinguino.java

--- Projecte_Pingu/src/modelos/Pinguino.java
+++ Projecte_Pingu/src/modelos/Pinguino.java
@@ -4,12 +4,13 @@ public class Pinguino extends jugador {
 	//ATRIBUTOS
 	protected Inventario inv;
 	
+	//CONSTRUCTOR
 	public Pinguino(String nom, int pos, Inventario inv) {
 		super(nom, pos);
 		this.inv = inv;
-		
 	}
 
+	//GETTERS Y SETTERS
 	public Inventario getInv() {
 		return inv;
 	}
@@ -18,20 +19,77 @@ public class Pinguino extends jugador {
 		this.inv = inv;
 	}
 	
+	//MÉTODO BATALLA()
 	public void batalla(Pinguino p) {
 		
 	}
 	
+	//MÉTODO USARITEM()
 	public void usarItem(item i) {
 		
 	}
 	
-	public void agregarItem(item i) {
-		
+	private int contarDados() {
+	    int total = 0;
+	    for (item it : inv.getInv()) {
+	        if (it.getNombre().contains("Normal")) {
+	            total++;
+	        }else if (it.getNombre().contains("Lento")) {
+	            total++;
+	        }
+	        else if (it.getNombre().contains("Rapido")) {
+	            total++;
+	        }
+	    }
+	    return total;
 	}
 	
-	public void quitarItem(item i) {
+	//MÉTODO AGREGARITEM
+	public void agregarItem(item i) {
+		boolean encontrado = false;
 		
+		if(i == null) {
+			System.out.println("No se puede añadir este item.");
+		}else {
+			String n = i.getNombre();
+			for(item exist : inv.getInv()) {
+				if(exist instanceof Dado) {
+					int total = contarDados();
+					if(total == 3) {
+						//No añade el dado
+					}else {
+						if(exist.getNombre().equalsIgnoreCase(i.getNombre()) && !exist.getNombre().equals("Normal")){
+							encontrado = true;
+							int nuevaCantidad = exist.getCantidad() + i.getCantidad();
+							exist.setCantidad(nuevaCantidad);
+						}
+					}
+				}else if(exist.getNombre().equalsIgnoreCase(i.getNombre()) && !(exist instanceof Dado)) {
+				encontrado = true;
+				int nuevaCantidad = exist.getCantidad() + i.getCantidad();
+				exist.setCantidad(nuevaCantidad);
+				}
+			}
+			if(!encontrado) {
+				inv.getInv().add(i);
+			}
+		}
 	}
-
+	
+	//MÉTODO QUITARITEM()
+	public void quitarItem(item i, int cantidad) {
+		if(i == null) {
+			System.out.println("No se puede quitar este item.");
+		}else {
+			for(item exist : inv.getInv()) {
+				if(exist.getNombre().equalsIgnoreCase(i.getNombre())){
+					if(exist.getCantidad() >= 1) {
+						exist.setCantidad(exist.getCantidad() - cantidad);
+					}else if(exist.getCantidad() <= 0) {
+						inv.getInv().remove(i);
+					}
+				}
+			}  
+		}
+	}
 }
