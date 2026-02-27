653f9de99d59334cb628fcbabd7ccb1912035409
carloss-a <alejandrofrost12@alumnes.ilerna.com>
2026-02-27 16:49:31 +0100

    Update Agujero.java

--- Projecte_Pingu/src/modelos/Agujero.java
+++ Projecte_Pingu/src/modelos/Agujero.java
@@ -1,13 +1,31 @@
 package modelos;
 
+import java.util.ArrayList;
+
 public class Agujero extends casilla {
-	//CONSTRUCTOR
-	public Agujero(int pos) {
-		super(pos);
-	}
-	
-	@Override
-	public void realizarAccion(Partida partida, Pinguino p) {
-		
-	}
+    //CONSTRUCTOR
+    public Agujero(int pos) {
+        super(pos);
+    }
+
+    @Override
+    public void realizarAccion(Partida partida, Pinguino p) {
+        if(partida != null && p != null) {
+            ArrayList<casilla> tablero = partida.getTablero().getCasilla();
+            int actual = p.getPos();
+
+            // Buscamos el agujero ANTERIOR (retrocediendo)
+            for(int i = actual - 1; i >= 0; i--) {  
+                if(tablero.get(i) instanceof Agujero) {
+                    p.setPos(i);
+                    i = -1;  
+                }
+            }
+
+            if(p.getPos() == actual) {  
+                p.setPos(1);
+            
+            }
+        }
+    }
 }
