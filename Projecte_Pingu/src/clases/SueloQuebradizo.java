504b413c316ed4497b125c95e378ff1e0dd832ed
Marcelo_Renteria <fmrrensu2004@alumnes.ilerna.com>
2026-03-02 17:04:44 +0100

    Hechos los Override's y Tablero

--- Projecte_Pingu/src/clases/SueloQuebradizo.java
+++ Projecte_Pingu/src/clases/SueloQuebradizo.java
@@ -5,10 +5,19 @@ public class SueloQuebradizo extends Casilla {
 	public SueloQuebradizo(int pos) {
 		super(pos);
 	}
+	
+	
 
 	@Override
 	public void realizarAccion(Partida p, Jugador j) {
-		
+		if(j instanceof Pinguino) {
+			Pinguino pingu =(Pinguino)j;
+			if(pingu.getInv().totalItems() > 5) {
+				pingu.setPos(0);
+			}else if(pingu.getInv().totalItems() <= 5 && pingu.getInv().totalItems() != 0) {
+				pingu.perderTurno();
+			}
+		}
 	}
 
 	
