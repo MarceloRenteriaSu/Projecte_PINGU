ded608b7c335bcd91d826205ce5fc9926fcc9eda
DenisTineo <frezzado@alumnes.ilerna.com>
2026-03-12 16:59:23 +0100

    creacion de las classes de vista

---
+++ Projecte_Pingu/src/VISTAS/PantallaPartida.java
@@ -0,0 +1,32 @@
+package VISTAS;
+
+public class PantallaPartida {
+
+	public void botonTirarDado() {
+		
+	}
+	
+	public void botonUsarObjeto() {
+			
+	}
+	
+	public void botonFibalizarTurno() {
+		
+	}
+	
+	public void iniciarPartida() {
+		
+	}
+	
+	public void cargarPartida() {
+		
+	}
+	
+	public void guardarPartida() {
+		
+	}
+	
+	public void refrescarPartida() {
+		
+	}
+}
